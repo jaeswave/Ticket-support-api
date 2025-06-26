afe8d1e491e4589e455bbde1f49b39140659da38
Oladele Tijani <jaeswave@gmail.com>
2025-06-26 09:36:21 +0100

    Initial commit or CORS fix

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -14,3 +14,16 @@
 #       methods: [:get, :post, :put, :patch, :delete, :options, :head]
 #   end
 # end
+
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins 'http://localhost:5173'  # 👈 allow your frontend's dev URL
+  
+      resource '*',
+        headers: :any,
+        methods: [:get, :post, :put, :patch, :delete, :options, :head],
+        expose: ['Authorization']
+    end
+  end
+  
